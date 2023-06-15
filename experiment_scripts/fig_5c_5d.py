6934146e49b7ddfeb1016620fae8230f638bfaff
Menachem Kalmenson <kalmenson.mm@gmail.com>
2023-06-15 04:41:15 -0400

    Update fig_5c_5d.py

--- experiment_scripts/fig_5c_5d.py
+++ experiment_scripts/fig_5c_5d.py
@@ -4,7 +4,6 @@ import numpy as np
 path_parent = os.path.dirname(os.getcwd())
 os.chdir(path_parent)
 sys.path.append(path_parent)
-
 import peak_ca_plus_plasticity_for_three_locations as pcp
 
 max_synapses = 40#maximum number of synapses per cluster, so in total there will be 2*max_synapses
